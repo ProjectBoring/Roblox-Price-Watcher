f27444c1f3d429910671061adf857f4f4f74a34f
ProjectBoring <73672695+ProjectBoring@users.noreply.github.com>
2025-03-06 20:24:56 -0600

    Update WebHandling.h

--- Roblox Price Watcher v1/WebHandling.h
+++ Roblox Price Watcher v1/WebHandling.h
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <curl/curl.h>
+#include "ImageLoading.h"
+#include "PriceWatchingEnergySaver.h"
+#include "vector"
 
 class WebHandler {
 public:
@@ -11,6 +14,8 @@ public:
     std::string post(const std::string& url, const std::string& json_body, const std::string& cookie = "", const std::string& xsrf_token = "");
     std::string get(const std::string& url); // Added GET method
     std::string getXSRFToken(const std::string& cookie);
+
+    Asset fetchAssetInfo(long long asset_id, const std::string& cookie, const std::string& xsrf_token);
 
 private:
     CURL* curl;
